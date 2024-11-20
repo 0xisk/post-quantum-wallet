b0bdd4f67d71eb198a56b82bcc70e90e4f1e8b22
0xisk <0xisk@proton.me>
2024-11-20 16:02:30 +0100

    chore: update trampoline

--- packages/bundler2/packages/bundler/hardhat.config.ts
+++ packages/bundler2/packages/bundler/hardhat.config.ts
@@ -38,6 +38,11 @@ const config: HardhatUserConfig = {
       url: 'http://localhost:8545/',
       saveDeployments: false
     },
+    sepoliaFork: {
+      url: 'http://127.0.0.1:8545',
+      chainId: 11011,
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
+    },
     goerli: getInfuraNetwork('goerli')
   },
   solidity: {
